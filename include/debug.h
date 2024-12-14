2506a9a7b359691a017d3405114479e0b80f69e6
snnbyyds <snnbyyds@gmail.com>
2024-12-14 21:37:19 +0800

    semu: include: Update Log color
    Change-Id: Ib0ff1427dc6fc7426a91c188f89b6457c3fefbbf

--- include/debug.h
+++ include/debug.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 #define Log(format, ...) \
-  printf("\33[1;35m[%s,%d,%s] " format "\33[0m\n", \
+  printf("\33[1;97m[%s,%d,%s] " format "\33[0m\n", \
       __FILE__, __LINE__, __func__, ## __VA_ARGS__)
 
 #define Info(format, ...) \
