88eebde62024d50a89f3681ad4f6d79cb10a27bf
snnbyyds <snnbyyds@gmail.com>
2024-12-14 21:30:16 +0800

    semu: device: Handle SDL_QUIT event
    Change-Id: Ieda61e7ccb5a8ac2d36730e39b18319c9f749e49

--- src/device/device.c
+++ src/device/device.c
@@ -1,5 +1,7 @@
 #include <device/device.h>
 #include <utils/timer.h>
+#include <utils/state.h>
+#include <SDL3/SDL.h>
 
 void init_serial();
 void init_timer();
@@ -9,10 +11,18 @@ void update_screen();
 void update_device() {
     static uint64_t pre = 0;
     static uint64_t cur = -1;
+    static SDL_Event event;
+
     cur = get_uptime();
     if (cur - pre > 1000000 / 60) {
         pre = cur;
         update_screen();
+        if (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT: Log("Received SDL quit event!"); SET_STATE(QUIT); break;
+                default: break;
+            }
+        }
     }
 }
 
