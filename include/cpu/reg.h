055ff7d17f3d2fba003aef36869a0fe96f5a583c
snnbyyds <snnbyyds@gmail.com>
2024-12-15 08:48:59 +0800

    semu: cpu: Declare several CSRs
    Change-Id: I147f394dc45509d4e846d7f521b426cc067a09ca

--- include/cpu/reg.h
+++ include/cpu/reg.h
@@ -4,6 +4,34 @@
 #include <common.h>
 
 #define gpr(i) cpu.gpr[i]
+#define csr(i) cpu.csr[i]
+
+typedef enum {
+    satp     = 0x180,
+    mstatus  = 0x300,
+    mtvec    = 0x305,
+    mscratch = 0x340,
+    mepc     = 0x341,
+    mcause   = 0x342
+} CSR_Number;
+
+typedef struct {
+    const char *name;
+    CSR_Number number;
+    const bool implemented;
+} CSR_Property;
+
+typedef union __attribute__((packed)) {
+  struct { word_t PPN : 22, ASID : 9, MODE : 1; } dec_SATP;
+  struct { word_t UIE: 1, SIE: 1, WPRI: 1, MIE: 1, UPIE: 1, SPIE: 1, WPRI2: 1, MPIE: 1,
+                  SPP: 1, WPRI3: 2, MPP: 2, FS: 2, XS: 2, MPRV: 1, SUM: 1, MXR: 1, TVM: 1,
+                  TW: 1, TSR: 1, WPRI4: 8, SD: 1; } dec_MSTATUS;
+  struct { word_t VALUE : 32; } dec_MTVEC;
+  struct { word_t VALUE : 32; } dec_MSCRATCH;
+  struct { word_t VALUE : 32; } dec_MEPC;
+  struct { word_t EXCEPTION : 31, INTERRUPT : 1; } dec_MCAUSE;
+  word_t val;
+} SR_Decode;
 
 void reg_val_display();
 
--- src/cpu/reg.c
+++ src/cpu/reg.c
@@ -1,15 +1,32 @@
 #include <cpu/cpu.h>
 #include <cpu/reg.h>
 
-static const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+static const char *gprs[] = {
+    "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+    "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+    "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+    "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+#define DECLARE_NEW_CSR(NAME, IMPLEMENT) [NAME] = { tostr(NAME), NAME, IMPLEMENT },
+
+static const CSR_Property csrs[4096] = {
+    DECLARE_NEW_CSR(satp, true)
+    DECLARE_NEW_CSR(mstatus, true)
+    DECLARE_NEW_CSR(mtvec, true)
+    DECLARE_NEW_CSR(mscratch, true)
+    DECLARE_NEW_CSR(mepc, true)
+    DECLARE_NEW_CSR(mcause, true)
 };
 
 void reg_val_display() {
-    for (size_t i = 0; i < LENGTH(regs); i++) {
-        printf("%-4s\t0x%08x\t%" PRIword "\n", regs[i], gpr(i), gpr(i));
+    for (size_t i = 0; i < LENGTH(gprs); i++) {
+        printf("%-4s\t0x%08x\t%" PRIword "\n", gprs[i], gpr(i), gpr(i));
+    }
+    putchar('\n');
+    for (size_t i = 0; i < LENGTH(csrs); i++) {
+        if (csrs[i].implemented) {
+            printf("%-9s\t0x%03x\t%" PRIword "\n", csrs[i].name, csrs[i].number, csr(i));
+        }
     }
 }
