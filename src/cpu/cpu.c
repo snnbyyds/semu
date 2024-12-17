cea10fe5aa07de2ddf787676747b2552b0338c9d
snnbyyds <snnbyyds@gmail.com>
2024-12-17 16:26:01 +0800

    semu: cpu: Use SCHED_RR for cpu_exec_thread()
    Change-Id: I57dda4687b2649f453d9f6eeea86f4056d34740e

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -13,8 +13,6 @@
 
 CPU_State cpu = {};
 
-static pthread_t thread_cpu_exec;
-
 static const uint32_t builtin_img[] = {
     0x00000297, // auipc t0,0
     0x00028823, // sb  zero,16(t0)
@@ -22,6 +20,10 @@ static const uint32_t builtin_img[] = {
     0x00100073, // ebreak
     0x00000000
 };
+
+static pthread_t thread_cpu_exec;
+static pthread_attr_t attr;
+static struct sched_param param;
 
 static inline void exec_once() {
     // exec the inst
@@ -48,7 +50,7 @@ static void *cpu_exec_thread(void *arg) {
 }
 
 static inline void start_cpu_exec_thread(uint64_t nr_exec) {
-    int rc = pthread_create(&thread_cpu_exec, NULL, cpu_exec_thread, (void *)nr_exec);
+    int rc = pthread_create(&thread_cpu_exec, &attr, cpu_exec_thread, (void *)nr_exec);
     if (rc) {
         Error("Failed to create cpu_exec thread!");
         assert(0);
@@ -61,15 +63,26 @@ static inline void wait_cpu_exec_thread() {
     }
 }
 
+static inline void halt_cpu() {
+    stop_timers();
+    pthread_attr_destroy(&attr);
+}
+
 void init_cpu(bool img_builtin) {
     memset(&cpu, 0, sizeof(cpu));
     cpu.pc = CONFIG_RESET_VECTOR;
     csr(mstatus) = 0x1800;
-    Log("CPU Initialized");
     if (img_builtin) {
         Log("Loading builtin image to 0x%" PRIx64 "...", (uint64_t)CONFIG_RESET_VECTOR);
         memcpy(guest_to_host(CONFIG_RESET_VECTOR), builtin_img, sizeof(builtin_img));
     }
+    // Set up thread
+    pthread_attr_init(&attr);
+    pthread_attr_setschedpolicy(&attr, SCHED_RR);
+    param.sched_priority = sched_get_priority_max(SCHED_RR);
+    pthread_attr_setschedparam(&attr, &param);
+    Log("Use SCHED_RR for cpu_exec_thread %p", cpu_exec_thread);
+    Log("CPU Initialized!");
 }
 
 void cpu_exec(uint64_t step) {
@@ -83,9 +96,9 @@ void cpu_exec(uint64_t step) {
 
     switch (semu_state.state) {
         case RUNNING: SET_STATE(STOP); stop_timers(); break;
-        case END: Info("Hit Good Trap at PC 0x%08" PRIaddr "", semu_state.halt_pc); stop_timers(); break;
-        case ABORT: Error("Hit Bad Trap at PC 0x%08" PRIaddr "", cpu.pc); stop_timers(); break;
-        case QUIT: stop_timers(); break;
+        case END: Info("Hit Good Trap at PC 0x%08" PRIaddr "", semu_state.halt_pc); halt_cpu(); break;
+        case ABORT: Error("Hit Bad Trap at PC 0x%08" PRIaddr "", cpu.pc); halt_cpu(); break;
+        case QUIT: halt_cpu(); break;
         default: break;
     }
 }
