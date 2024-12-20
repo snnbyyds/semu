5c50f6f0d4314e381a468d74aa172e22faf53324
snnbyyds <snnbyyds@gmail.com>
2024-12-21 07:04:22 +0800

    semu: device: mmio: Use binarysearch for findmap()
    Change-Id: I1e3ca7255df1ddb785d17f80183e3bf0bd2aed05

--- src/device/mmio/mmio.c
+++ src/device/mmio/mmio.c
@@ -20,6 +20,10 @@ static inline uint8_t *new_iospace(size_t size) {
     return ret;
 }
 
+static int mapcmp(const void *a, const void *b) {
+    return ((IOMap *)a)->low > ((IOMap *)b)->low ? 1 : -1;
+}
+
 #define CHECK_OVERLAP(IOADDR) \
     if ((IOADDR) >= CONFIG_MBASE && (IOADDR) < CONFIG_MBASE + CONFIG_MSIZE) { Error("ioaddr 0x%" PRIaddr " overlapped!", (ioaddr_t)(IOADDR)); Assert(0); }
 
@@ -35,16 +39,25 @@ void *add_iomap(ioaddr_t start, size_t size, io_handler_t handler) {
     }
     void *ret = new_iospace(size);
     maps[nr_maps++] = (IOMap) { .low = start, .high = start + size - 1, .io_handler = handler, .space = ret };
+    qsort(maps, nr_maps, sizeof(IOMap), mapcmp);
     Log("Added mmio map at 0x%" PRIaddr "", start);
     return ret;
 }
 
 __attribute__((always_inline))
 static inline int findmap(ioaddr_t addr) {
-    for (int i = 0; i < nr_maps; i++) {
-        if (addr >= maps[i].low && addr <= maps[i].high) {
-            return i;
+    int left = 0;
+    int right = nr_maps - 1;
+    while (left <= right) {
+        int mid = (left + right) >> 1;
+        if (maps[mid].low <= addr) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
+    }
+    if (right >= 0 && right < nr_maps && maps[right].high >= addr && maps[right].low <= addr) {
+        return right;
     }
     return -1;
 }
