77b3010e8424f141237ac72fea83dc789020a3ee
snnbyyds <snnbyyds@gmail.com>
2025-01-02 22:42:27 +0800

    semu: Relocate gdbstub

--- src/monitor/filelist.mk
+++ src/monitor/filelist.mk
@@ -1,13 +1,9 @@
-ifneq ($(wildcard repo/include/gdbstub.h),)
-	DIRS-BLACKLIST-y += src/monitor/gdbstub/repo/tests src/monitor/gdbstub/repo
-endif
-
 ifeq ($(CONFIG_USE_GDBSTUB),)
 SRCS-BLACKLIST-y += src/monitor/gdbstub/gdbstub.c
 else
-LIBGDBSTUB = src/monitor/gdbstub/repo/build/libgdbstub.a
-CFLAGS += -I src/monitor/gdbstub/repo/include -Wno-unused-variable
+LIBGDBSTUB = tools/gdbstub/repo/build/libgdbstub.a
+CFLAGS += -I tools/gdbstub/repo/include
 src/monitor/gdbstub/gdbstub.c: $(LIBGDBSTUB)
 $(LIBGDBSTUB):
-	$(MAKE) -C src/monitor/gdbstub
+	$(MAKE) -C tools/gdbstub
 endif
